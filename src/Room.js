3c83bae6ca145d93aa9e3a4cf26bf836a40f55c5
Ayesha116 <ayeshajawed1168@gmail.com>
2020-06-13 17:18:16 +0500

    stylized room.js file

--- src/Room.js
+++ src/Room.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
+import "./Room.css"
 
 
 function Room() {
   let [isLit , setLit] = useState(false)  
+  let [temp , settemp] = useState(72)
       
   return (
-    <div>
-        <div>the room is {isLit ? 'lit' : 'dark'}</div>
-        <button onClick = {()=>setLit(!isLit)} >Toggle light</button>
+    <div className = {`room ${isLit? "lit": "dark"}`}>
+        <br/><br/><br/>
+        <div >the room is {isLit ? 'lit' : 'dark'}</div>
+        <button onClick = {()=>setLit(true)} >On</button>
+        <button onClick = {()=>setLit(false)} >Off</button>
+        <br/><br/><br/>
+        <div>the temperature is {temp}</div>
+        
+        <button onClick = {()=>settemp(--temp)}>-</button>
+        <button onClick = {()=>settemp(++temp)}>+</button>
     </div>
   )
   
